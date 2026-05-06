135546b2a89453e4458f83ebf099de77e11b56cf
xrssr2807 <18996092807@163.com>
2026-05-06 16:31:20 +0800

    下游微调修改

--- MIRA/models/modeling_mira.py
+++ MIRA/models/modeling_mira.py
@@ -495,7 +495,7 @@ class MIRAAttention(nn.Module):
         key_states = key_states.view(bsz, q_len, self.num_key_value_heads, self.head_dim).transpose(1, 2)
         value_states = value_states.view(bsz, q_len, self.num_key_value_heads, self.head_dim).transpose(1, 2)
         if past_key_value is not None:
-            past_len = past_key_value.get_usable_length(None, self.layer_idx)
+            past_len = past_key_value.get_seq_length() if hasattr(past_key_value, 'get_seq_length') else past_key_value.get_usable_length(None, self.layer_idx)
             kv_seq_len = past_len + key_states.shape[-2]
             if self.layer_idx is None:
                 raise ValueError(
@@ -503,7 +503,7 @@ class MIRAAttention(nn.Module):
                     "for auto-regressive decoding with k/v caching, please make sure to initialize the attention class "
                     "with a layer index."
                 )
-            kv_seq_len += past_key_value.get_usable_length(kv_seq_len, self.layer_idx)
+            kv_seq_len += past_key_value.get_seq_length() if hasattr(past_key_value, 'get_seq_length') else past_key_value.get_usable_length(kv_seq_len, self.layer_idx)
         else:
             kv_seq_len = key_states.shape[-2]
         
@@ -630,7 +630,7 @@ class MIRAFlashAttention2(MIRAAttention):
                     "for auto-regressive decoding with k/v caching, please make sure to initialize the attention class "
                     "with a layer index."
                 )
-            kv_seq_len += past_key_value.get_usable_length(kv_seq_len, self.layer_idx)
+            kv_seq_len += past_key_value.get_seq_length() if hasattr(past_key_value, 'get_seq_length') else past_key_value.get_usable_length(kv_seq_len, self.layer_idx)
         rotary_seq_len = max(kv_seq_len, position_ids[:, -1].max().item()) + 1
         cos, sin = self.rotary_emb(value_states, seq_len=rotary_seq_len)
         query_states, key_states = apply_rotary_pos_emb(query_states, key_states, cos, sin, position_ids)
@@ -963,7 +963,7 @@ class MIRAModel(MIRAPreTrainedModel):
                     past_key_values = DynamicCache.from_legacy_cache(past_key_values)
                 else:
                     past_key_values = DynamicCache()
-            past_key_values_length = past_key_values.get_usable_length(seq_length)
+            past_key_values_length = past_key_values.get_seq_length() if hasattr(past_key_values, 'get_seq_length') else past_key_values.get_usable_length(seq_length)
 
         if position_ids is None:
             device = input_ids.device if input_ids is not None else inputs_embeds.device
